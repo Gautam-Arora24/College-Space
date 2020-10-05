8d3668dfd82d1ff2e2d36ce1aeececd31a78cee0
Gautam Arora <gautamarora6248@gmail.com>
2020-10-06 01:03:07 +0530

    version 1.0.2

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static("./client/build"));
 
 // DATABASE CONNECTION
 mongoose.connect(
-  process.env.DATABASE,
+  "mongodb+srv://gautam:6kiuz209MjGa3uvk@cluster0.fgbkk.mongodb.net/CollegeSpace?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (error) {
     if (error) console.log(error);
